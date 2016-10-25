0adc676d569c7a62a04affc921cc7bd71592fbc0
leiwang <wang.bonn@gmail.com>
2016-10-25 12:28:47 +0200

    fix postinstall

--- bin/install-swagger.js
+++ bin/install-swagger.js
@@ -1,18 +1,17 @@
 var fs = require('fs-extra');
 var replace = require("replace");
-var cwd = require('path').resolve();
 var path = require('path');
 
 try {
-  const from = path.join(cwd, './node_modules/swagger-ui/dist');
-  const to = path.join(cwd, './public/swagger');
+  const from = path.join('./node_modules/swagger-ui/dist');
+  const to = path.join('./public/swagger');
   console.log('copy from %s, to %s', from, to);
   fs.copySync(from, to);
 
   replace({
     regex: '("api_key", key, "query")',
     replacement: '("Authorization", "JWT " + key, "header")',
-    paths: ['./public/swagger/index.html'],
+    paths: [path.join('./public/swagger/index.html')],
     recursive: false,
     silent: true
   });
